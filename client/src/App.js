b72f736ac99de3e4edf3ef8a18c0da39bf7e52e6
baegofda_ <65588499+baegofda@users.noreply.github.com>
2021-03-01 08:09:36 +0900

    modify header css

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,12 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 
-function App() {
-  return <Header />;
-}
+const App = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
 
 export default App;
--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,17 +1,65 @@
-import React, { useEffect } from "react";
-import axios from "axios";
+import React from "react";
+import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
 
 const Header = (props) => {
-  const callApi = () => {
-    axios.get("/api").then((res) => console.log(res.data.test));
-  };
-
-  useEffect(() => {
-    callApi();
-  }, []);
-
-  return <div className={styles.test}>hi</div>;
+  return (
+    <header className={styles.header}>
+      <h1 className={styles.title}>
+        <Link to="/">
+          <img src="" alt="" />
+        </Link>
+      </h1>
+      <nav>
+        <ul>
+          <li>
+            <h2>국내</h2>
+            <ul>
+              <li>
+                <Link to="#">국내 종합 현황</Link>
+              </li>
+              <li>
+                <Link to="#">국내 시도별 현황</Link>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <h2>해외</h2>
+            <ul>
+              <li>
+                <Link to="#">해외 종합 현황</Link>
+              </li>
+              <li>
+                <Link to="#">해외 주변 국가별 현황</Link>
+              </li>
+              <li>
+                <Link to="#">전 세계 대시보드</Link>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <h2>정보</h2>
+            <ul>
+              <li>
+                <Link to="#">코로나19 정보</Link>
+              </li>
+              <li>
+                <Link to="#">주요 뉴스</Link>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <h2>기타</h2>
+            <ul>
+              <li>
+                <Link to="#">데이터 출처</Link>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
