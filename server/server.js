12069dd6fea9a967082d374228611881e10e5212
baegofda_ <65588499+baegofda@users.noreply.github.com>
2021-03-01 06:10:07 +0900

    modify client folder

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const test = require("./Router/test");
 
-app.use("/", test);
+app.use("/api/", test);
 
 const port = 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
