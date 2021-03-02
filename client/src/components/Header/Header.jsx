16ccf750bd20bb6d7af0ff39e4987b417da7fd1b
baegofda_ <65588499+baegofda@users.noreply.github.com>
2021-03-03 02:35:52 +0900

    add gnb toggle

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,28 +1,52 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import styles from "./Header.module.css";
 const LOGO = "/assets/images/main-logo.png";
 
 const Header = (props) => {
+  const [isToggle, setIsToggle] = useState(false);
+  const gnb = useRef();
+  const toggle = isToggle ? styles.toggle : "";
+
+  const onToggle = (e) => {
+    e.preventDefault();
+    setIsToggle(isToggle ? false : true);
+  };
+
   return (
     <header className={styles.header}>
-      <Link className={styles.gnb} to="#none">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 448 512"
-          fill="#000"
-          width="24"
-          height="24"
-        >
-          <path d="M12.83 352h262.34A12.82 12.82 0 0 0 288 339.17v-38.34A12.82 12.82 0 0 0 275.17 288H12.83A12.82 12.82 0 0 0 0 300.83v38.34A12.82 12.82 0 0 0 12.83 352zm0-256h262.34A12.82 12.82 0 0 0 288 83.17V44.83A12.82 12.82 0 0 0 275.17 32H12.83A12.82 12.82 0 0 0 0 44.83v38.34A12.82 12.82 0 0 0 12.83 96zM432 160H16a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16zm0 256H16a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16z" />
-        </svg>
-      </Link>
+      <span className={styles.gnb}>
+        <Link ref={gnb} className={styles.gnbToggle} to="#" onClick={onToggle}>
+          {isToggle ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="#000"
+              width="24"
+              height="24"
+            >
+              <path fill="none" d="M0 0h24v24H0z" />
+              <path d="M12 10.586l4.95-4.95 1.414 1.414-4.95 4.95 4.95 4.95-1.414 1.414-4.95-4.95-4.95 4.95-1.414-1.414 4.95-4.95-4.95-4.95L7.05 5.636z" />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 448 512"
+              fill="#000"
+              width="24"
+              height="24"
+            >
+              <path d="M12.83 352h262.34A12.82 12.82 0 0 0 288 339.17v-38.34A12.82 12.82 0 0 0 275.17 288H12.83A12.82 12.82 0 0 0 0 300.83v38.34A12.82 12.82 0 0 0 12.83 352zm0-256h262.34A12.82 12.82 0 0 0 288 83.17V44.83A12.82 12.82 0 0 0 275.17 32H12.83A12.82 12.82 0 0 0 0 44.83v38.34A12.82 12.82 0 0 0 12.83 96zM432 160H16a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16zm0 256H16a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16z" />
+            </svg>
+          )}
+        </Link>
+      </span>
       <h1 className={styles.logo}>
         <Link className={styles.logoLink} to="/">
           <img className={styles.img} src={LOGO} alt="겟아웃코로나" />
         </Link>
       </h1>
-      <nav className={styles.nav}>
+      <nav className={`${styles.nav} ${toggle}`}>
         <ul className={styles.navContainer}>
           <li className={styles.categoryContainer}>
             <h2 className={styles.category}>국내</h2>
