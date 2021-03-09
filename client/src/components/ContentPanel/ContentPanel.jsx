85a4b8291b193457d67d4e5bd22389ad7fa82526
baegofda_ <65588499+baegofda@users.noreply.github.com>
2021-03-09 22:53:29 +0900

    success panel data

--- client/src/components/ContentPanel/ContentPanel.jsx
+++ client/src/components/ContentPanel/ContentPanel.jsx
@@ -1,36 +1,18 @@
 import React from "react";
 import styles from "./ContentPanel.module.css";
 
-const ContentPanel = () => {
+const ContentPanel = ({ data }) => {
   return (
     <section className={styles.wrap}>
       <h3 className="sr-only">국내 종합 현황 수</h3>
       <dl className={styles.panel}>
-        <div className={styles.card}>
-          <dt className={styles.title}>확진자 수</dt>
-          <dd className={styles.count}>12345677</dd>
-          <dd className={styles.sub}>+ 555</dd>
-        </div>
-        <div className={styles.card}>
-          <dt className={styles.title}>검사 진행</dt>
-          <dd className={styles.count}>12345677</dd>
-          <dd className={styles.sub}>+ 3232</dd>
-        </div>
-        <div className={styles.card}>
-          <dt className={styles.title}>치료 중</dt>
-          <dd className={styles.count}>12345677</dd>
-          <dd className={styles.sub}>+ 422</dd>
-        </div>
-        <div className={styles.card}>
-          <dt className={styles.title}>완치자 수</dt>
-          <dd className={styles.count}>12345677</dd>
-          <dd className={styles.sub}>+ 444</dd>
-        </div>
-        <div className={styles.card}>
-          <dt className={styles.title}>사망자 수</dt>
-          <dd className={styles.count}>12345677</dd>
-          <dd className={styles.sub}>+ 33</dd>
-        </div>
+        {data.map((item) => (
+          <div key={item.id} className={styles.card}>
+            <dt className={styles.title}>{item.title}</dt>
+            <dd className={styles.count}>{item.count}</dd>
+            <dd className={styles.sub}>+ {item.new}</dd>
+          </div>
+        ))}
       </dl>
     </section>
   );
--- client/src/components/KoreaData/KoreaAllData/KoreaAllData.jsx
+++ client/src/components/KoreaData/KoreaAllData/KoreaAllData.jsx
@@ -9,15 +9,46 @@ const KoreaAllData = () => {
     title: "국내 종합 현황",
     desc: "국내 코로나 종합 현황판과 일주일 현황 차트를 제공합니다.",
   });
-  const [data, setData] = useState({});
+  const [panelData, setPanelData] = useState([]);
+
+  const panelDataHandler = (totalData, yesterDayData) => {
+    const totalDecideCnt = totalData[7].elements[0].text;
+    const totalExamCnt = totalData[8].elements[0].text;
+    const totalCareCnt = totalData[3].elements[0].text;
+    const totalClearCnt = totalData[4].elements[0].text;
+    const totalDeathCnt = totalData[6].elements[0].text;
+
+    const yesterDayDecideCnt = yesterDayData[7].elements[0].text;
+    const yesterDayExamCnt = yesterDayData[8].elements[0].text;
+    const yesterDayCareCnt = yesterDayData[3].elements[0].text;
+    const yesterDayClearCnt = yesterDayData[4].elements[0].text;
+    const yesterDayDeathCnt = yesterDayData[6].elements[0].text;
+
+    const newDecideCnt = totalDecideCnt - yesterDayDecideCnt;
+    const newDayExamCnt = totalExamCnt - yesterDayExamCnt;
+    const newCareCnt = totalCareCnt - yesterDayCareCnt;
+    const newClearCnt = totalClearCnt - yesterDayClearCnt;
+    const newDeathCnt = totalDeathCnt - yesterDayDeathCnt;
+    setPanelData([
+      { id: "1", title: "확진자 수", count: totalDecideCnt, new: newDecideCnt },
+      { id: "2", title: "검사진행", count: totalExamCnt, new: newDayExamCnt },
+      { id: "3", title: "치료 중", count: totalCareCnt, new: newCareCnt },
+      { id: "4", title: "완치자 수", count: totalClearCnt, new: newClearCnt },
+      { id: "5", title: "사망자 수", count: totalDeathCnt, new: newDeathCnt },
+    ]);
+  };
 
   useEffect(() => {
     const callApi = async () => {
       axios
         .get("/api")
-        .then((res) =>
-          console.log(res.data.elements[0].elements[1].elements[0].elements)
-        )
+        .then((res) => {
+          const totalData =
+            res.data.elements[0].elements[1].elements[0].elements[0].elements;
+          const yesterDayData =
+            res.data.elements[0].elements[1].elements[0].elements[1].elements;
+          panelDataHandler(totalData, yesterDayData);
+        })
         .catch((err) => console.log(err));
     };
 
@@ -27,7 +58,7 @@ const KoreaAllData = () => {
   return (
     <>
       <ContentTitle data={title} />
-      <ContentPanel />
+      <ContentPanel data={panelData} />
     </>
   );
 };
