a0d71cbf9184a338f559a5edce0a39ce6ab51da9
baegofda_ <65588499+baegofda@users.noreply.github.com>
2021-03-11 06:32:09 +0900

    set news

--- client/src/components/News/News.jsx
+++ client/src/components/News/News.jsx
@@ -8,30 +8,38 @@ const News = () => {
     title: "주요 뉴스",
     desc: "코로나 백신과 관련된 주요 뉴스들을 보여줍니다.",
   });
+  const [news, setNews] = useState({});
 
   useEffect(() => {
     axios
       .get("/api/news")
-      .then((res) => console.log(res))
+      .then((res) => {
+        const items = res.data.items;
+        const newsTitle = items.map((item) => {
+          return item.title;
+        });
+        const newsLink = items.map((item) => {
+          return item.link;
+        });
+        setNews({
+          title: newsTitle,
+          link: newsLink,
+        });
+      })
       .catch((err) => console.log(err));
-  });
+  }, []);
 
   return (
     <>
       <ContentTitle data={title} />
       <section className={styles.wrap}>
-        <h3 className={styles.title}>네이버 뉴스 목록</h3>
+        <h3 className={styles.category}>네이버 뉴스 목록</h3>
         <ul className={styles.items}>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
+          <li>
+            <a href="">
+              <strong className={styles.title}></strong>-날짜
+            </a>
+          </li>
         </ul>
       </section>
     </>
