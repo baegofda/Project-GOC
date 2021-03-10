5df6dd2063c4ad6dcc483c6c630f48f0c24561b6
baegofda_ <65588499+baegofda@users.noreply.github.com>
2021-03-11 04:22:54 +0900

    modify background

--- client/src/components/OverseasData/OverseasAllData/OverseasAllData.jsx
+++ client/src/components/OverseasData/OverseasAllData/OverseasAllData.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import ContentTitle from "../../ContentTitle/ContentTitle";
 import styles from "./OverseasAllData.module.css";
 
 const OverseasAllData = () => {
-  return <div></div>;
+  const [title, setTitle] = useState({
+    title: "해외 종합 현황",
+    desc: "해외 코로나 종합 현황판과 일별 현황 차트를 제공합니다.",
+  });
+  return (
+    <>
+      <ContentTitle data={title} />
+      <div></div>
+    </>
+  );
 };
 
 export default OverseasAllData;
