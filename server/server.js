97100c81464412090c82cdc358ae3a6d0ec09b50
baegofda_ <65588499+baegofda@users.noreply.github.com>
2021-03-17 23:43:27 +0900

    yarn add build

--- server/server.js
+++ server/server.js
@@ -19,5 +19,5 @@ app.use("/api/news/daum", daumNewsRoute);
 app.use("/api/all", overseasAllRoute);
 app.use("/api/country", overseasCountryRoute);
 
-const port = 5000;
-app.listen(port, () => console.log(`Listening on port ${port}`));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
