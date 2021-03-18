aff5278720e937b3b3069e995840626e137f430c
baegofda_ <65588499+baegofda@users.noreply.github.com>
2021-03-18 13:47:04 +0900

    modify proxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = (app) => {
   app.use(
     createProxyMiddleware("/api", {
-      target: "http://localhost:5000",
+      target: "https://git.heroku.com/projectgoc.git",
       changeOrigin: true,
     })
   );
