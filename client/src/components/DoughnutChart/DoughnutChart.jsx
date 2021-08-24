b0f90b115548b20d766fb81beff2915d72462267
baegofda <molly.daleo3o@gmail.com>
2021-08-24 22:58:00 +0900

    도넛차트 NaN처리 로직 추가

--- client/src/components/DoughnutChart/DoughnutChart.jsx
+++ client/src/components/DoughnutChart/DoughnutChart.jsx
@@ -10,29 +10,31 @@ const DoughnutChart = ({ data, options }) => {
           <h3 className="sr-only">{options[item.key].title.text}</h3>
           <Doughnut data={item} options={options[item.key]} />
           <dl className={styles.legends}>
-            {item.labels.map((label, idx) => (
-              <div key={idx} className={styles.legend}>
-                <dt className={styles.label}>
-                  <span
-                    className={styles.color}
-                    style={{
-                      background: item.datasets[0].backgroundColor[idx],
-                    }}
-                  ></span>
-                  {label}
-                </dt>
-                <dd className={styles.cnt}>
-                  {item.datasets[0].data[idx].toLocaleString()} 명
-                  <span className={styles.per}>
-                    (
-                    {((item.datasets[0].data[idx] / item.total) * 100).toFixed(
-                      1
-                    )}
-                    %)
-                  </span>
-                </dd>
-              </div>
-            ))}
+            {item.labels.map((label, idx) => {
+              let percent = (
+                (item.datasets[0].data[idx] / item.total) *
+                100
+              ).toFixed(1);
+              return (
+                <div key={idx} className={styles.legend}>
+                  <dt className={styles.label}>
+                    <span
+                      className={styles.color}
+                      style={{
+                        background: item.datasets[0].backgroundColor[idx],
+                      }}
+                    ></span>
+                    {label}
+                  </dt>
+                  <dd className={styles.cnt}>
+                    {item.datasets[0].data[idx].toLocaleString()} 명
+                    <span className={styles.per}>
+                      ({isNaN(percent) ? 0 : percent}%)
+                    </span>
+                  </dd>
+                </div>
+              );
+            })}
           </dl>
         </article>
       ))}
